8cb22db40c7afd394e5b2f4e2792a08aff0c4fa4
kori <kori@gmail.com>
2025-02-24 20:07:10 +0500

    first

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Navbar from "./components/Navbar.jsx";
+import Hero from "./components/Hero.jsx";
+import Highlights from "./components/Highlights.jsx";
+import Model from "./components/Model.jsx";
+import Features from "./components/Features.jsx";
+import HowItWorks from "./components/HowItWorks.jsx";
+import Footer from "./components/Footer.jsx";
+
+const App = () => {
+    return (
+        <main className="bg-black">
+            <Navbar />
+            <Hero />
+            <Highlights />
+            <Model />
+            <Features />
+            <HowItWorks />
+            <Footer />
+        </main>
+    );
+};
+
+export default App;
---
+++ src/components/IPhone.jsx
@@ -0,0 +1,46 @@
+import * as THREE from 'three';
+import React, { useEffect } from "react";
+import { useGLTF, useTexture } from "@react-three/drei";
+
+const Model = (props) => {
+    const { nodes, materials } = useGLTF('/models/scene.glb');
+    const texture = props.item?.img ? useTexture(props.item.img) : null;
+
+    useEffect(() => {
+        Object.entries(materials).forEach(([name, material]) => {
+            if (!["zFdeDaGNRwzccye", "ujsvqBWRMnqdwPx", "hUlRcbieVuIiOXG", "jlzuBkUzuJqgiAK", "xNrofRCqOXXHVZt"].includes(name)) {
+                material.color.set(props.item.color[0]);
+            }
+            material.needsUpdate = true;
+        });
+    }, [materials, props.item]);
+
+    return (
+        <group {...props}>
+            {Object.entries(nodes).map(([key, node]) => (
+                <mesh
+                    key={key}
+                    castShadow
+                    receiveShadow
+                    geometry={node.geometry}
+                    material={materials[node.material?.name] || undefined}
+                    scale={0.01}
+                />
+            ))}
+            {texture && (
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.xXDHkMplTIDAXLN.geometry}
+                    scale={0.01}
+                >
+                    <meshStandardMaterial roughness={1} map={texture} />
+                </mesh>
+            )}
+        </group>
+    );
+};
+
+export default Model;
+
+useGLTF.preload("/models/scene.glb");
